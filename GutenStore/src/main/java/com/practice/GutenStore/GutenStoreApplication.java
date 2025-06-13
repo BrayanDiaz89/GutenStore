eb5c37ac5a208acd869b98e9cbbd543f92ef9a46
BrayanDiaz89 <brayandiaz258f@gmail.com>
2025-06-13 18:03:43 -0500

    Add project from Spring initialzr

---
+++ GutenStore/src/main/java/com/practice/GutenStore/GutenStoreApplication.java
@@ -0,0 +1,13 @@
+package com.practice.GutenStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutenStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutenStoreApplication.class, args);
+	}
+
+}
